05972607ea0c2f542d9187cfedcf6c3ecda1e33a
paiizz <wanchanapon.t@ku.th>
2018-06-01 11:01:43 +0700

    edit version

--- App.js
+++ App.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Platform,
   StyleSheet,
@@ -21,32 +21,32 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-const appUpdate = new AppUpdate({
-  iosAppId: '123456',
-  apkVersionUrl: 'https://github.com/version.json',
-  needUpdateApp: (needUpdate) => {
-    Alert.alert(
-      'Update tip',
-      'Finding new version, do you want to update?',
-      [
-        {text: 'Cancel', onPress: () => {}},
-        {text: 'Update', onPress: () => needUpdate(true)}
-      ]
-    );
-  },
-  forceUpdateApp: () => {
-    console.log("Force update will start")
-  },
-  notNeedUpdateApp: () => {
-    console.log("App is up to date")
-  },
-  downloadApkStart: () => { console.log("Start") },
-  downloadApkProgress: (progress) => { console.log(`Downloading ${progress}%...`) },
-  downloadApkEnd: () => { console.log("End") },
-  onError: () => { console.log("downloadApkError") }
-});
-appUpdate.checkUpdate();
-export default class App extends Component {
+// const appUpdate = new AppUpdate({
+//   iosAppId: '123456',
+//   apkVersionUrl: 'https://github.com/PaiizZ/testappupdate/blob/master/version.json',
+//   needUpdateApp: (needUpdate) => {
+//     Alert.alert(
+//       'Update tip',
+//       'Finding new version, do you want to update?',
+//       [
+//         {text: 'Cancel', onPress: () => {}},
+//         {text: 'Update', onPress: () => needUpdate(true)}
+//       ]
+//     );
+//   },
+//   forceUpdateApp: () => {
+//     console.log("Force update will start")
+//   },
+//   notNeedUpdateApp: () => {
+//     console.log("App is up to date")
+//   },
+//   downloadApkStart: () => { console.log("Start") },
+//   downloadApkProgress: (progress) => { console.log(`Downloading ${progress}%...`) },
+//   downloadApkEnd: () => { console.log("End") },
+//   onError: () => { console.log("downloadApkError") }
+// });
+// appUpdate.checkUpdate();
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
