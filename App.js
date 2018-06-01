29d59ca8efe9c0c5dd41f6e49b49ca7647411548
paiizz <wanchanapon.t@ku.th>
2018-06-01 10:45:39 +0700

    add version.json

--- App.js
+++ App.js
@@ -9,8 +9,10 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Alert
 } from 'react-native';
+import AppUpdate from 'react-native-appupdate';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,8 +21,32 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+const appUpdate = new AppUpdate({
+  iosAppId: '123456',
+  apkVersionUrl: 'https://github.com/version.json',
+  needUpdateApp: (needUpdate) => {
+    Alert.alert(
+      'Update tip',
+      'Finding new version, do you want to update?',
+      [
+        {text: 'Cancel', onPress: () => {}},
+        {text: 'Update', onPress: () => needUpdate(true)}
+      ]
+    );
+  },
+  forceUpdateApp: () => {
+    console.log("Force update will start")
+  },
+  notNeedUpdateApp: () => {
+    console.log("App is up to date")
+  },
+  downloadApkStart: () => { console.log("Start") },
+  downloadApkProgress: (progress) => { console.log(`Downloading ${progress}%...`) },
+  downloadApkEnd: () => { console.log("End") },
+  onError: () => { console.log("downloadApkError") }
+});
+appUpdate.checkUpdate();
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
